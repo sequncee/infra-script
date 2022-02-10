d6e96b40e69cbd4c01470ad8b500062d16275ce3
legiorange <legiorange@163.com>
2022-02-10 20:21:47 +0800

    add Generating a new SSH key

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # infra-script
+
+    Simple shell script for new system devolop env.
+
+# Functions:
+- 1.Generating a new SSH key and adding it to the ssh-agent.
---
+++ ssh/init_ssh.sh
@@ -0,0 +1,3 @@
+ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+# Note: If you are using a legacy system that doesn't support the Ed25519 algorithm, use:
+# ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
